c1f2a34af9e73c2a0cd6b1e7a9ed781566079b71
guilherme-gessele <gagessele@outlook.com.br>
2022-11-06 18:17:26 -0300

    feat: improved lexer for basic monkey code

--- monkey-lexer/src/lib.rs
+++ monkey-lexer/src/lib.rs
@@ -1,7 +1,14 @@
-use std::str::Chars;
+use std::{iter::Peekable, str::Chars};
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum Token {
+    Illegal,
+    EOF,
+
+    // Identifiers
+    Identifier(String),
+    Integer(usize),
+
     // Operators
     Assign,
     Plus,
@@ -14,18 +21,30 @@ pub enum Token {
     RightParenthesis,
     LeftBrace,
     RightBrace,
+
+    // Keywords
+    Let,
+    Function,
 }
 
 pub struct Lexer<'lexer> {
-    input: Chars<'lexer>,
+    input: Peekable<Chars<'lexer>>,
 }
 
 impl<'lexer> Lexer<'lexer> {
     pub fn new(input: &'lexer str) -> Self {
-        let input = input.chars().into_iter();
-        Lexer { 
-            input
+        let input = input.chars().into_iter().peekable();
+        Lexer { input }
+    }
+
+    fn read_until(&mut self, c: char, func: impl Fn(&char) -> bool) -> String {
+        let mut output = vec![c];
+
+        while let Some(c) = self.input.next_if(&func) {
+            output.push(c);
         }
+
+        String::from_iter(output)
     }
 }
 
@@ -33,10 +52,9 @@ impl<'lexer> Iterator for Lexer<'lexer> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        
-        let char = self.input.next();
+        let keyword = self.input.next();
 
-        match char {
+        match keyword {
             Some('=') => Some(Token::Assign),
             Some('+') => Some(Token::Plus),
             Some('(') => Some(Token::LeftParenthesis),
@@ -45,10 +63,24 @@ impl<'lexer> Iterator for Lexer<'lexer> {
             Some('}') => Some(Token::RightBrace),
             Some(',') => Some(Token::Comma),
             Some(';') => Some(Token::Semicolon),
-            Some(_) => None,
+            Some(c) if c.is_digit(10) => {
+                let str = self.read_until(c, |c| c.is_digit(10));
+
+                Some(Token::Integer(str.parse::<usize>().unwrap()))
+            }
+            Some(c) if c.is_ascii_alphabetic() => {
+                let str = self.read_until(c, |c| c.is_ascii_alphabetic());
+
+                match str.as_str() {
+                    "let" => Some(Token::Let),
+                    "fn" => Some(Token::Function),
+                    _ => Some(Token::Identifier(str)),
+                }
+            }
+            Some(c) if c.is_whitespace() => self.next(),
+            Some(_) => Some(Token::Illegal),
             None => None,
         }
-
     }
 }
 
@@ -59,13 +91,12 @@ mod tests {
     use super::Token;
 
     #[test]
-    fn next_token() {
-
+    fn delimiters() {
         let input = "=+(){},;";
         let lexer = Lexer::new(input);
-        
+
         let expected_tokens = vec![
-            Token::Assign, 
+            Token::Assign,
             Token::Plus,
             Token::LeftParenthesis,
             Token::RightParenthesis,
@@ -78,7 +109,61 @@ mod tests {
         let tokens = lexer.into_iter().collect::<Vec<Token>>();
 
         assert_eq!(expected_tokens, tokens);
-
     }
 
+    #[test]
+    fn basic_code() {
+        let input = "let five = 5;
+        let ten = 10;
+        let add = fn(x, y) {
+        x + y;
+        };
+        let result = add(five, ten);
+        ";
+
+        let lexer = Lexer::new(input);
+
+        let expected_tokens = vec![
+            Token::Let,
+            Token::Identifier(String::from("five")),
+            Token::Assign,
+            Token::Integer(5),
+            Token::Semicolon,
+            Token::Let,
+            Token::Identifier(String::from("ten")),
+            Token::Assign,
+            Token::Integer(10),
+            Token::Semicolon,
+            Token::Let,
+            Token::Identifier(String::from("add")),
+            Token::Assign,
+            Token::Function,
+            Token::LeftParenthesis,
+            Token::Identifier(String::from("x")),
+            Token::Comma,
+            Token::Identifier(String::from("y")),
+            Token::RightParenthesis,
+            Token::LeftBrace,
+            Token::Identifier(String::from("x")),
+            Token::Plus,
+            Token::Identifier(String::from("y")),
+            Token::Semicolon,
+            Token::RightBrace,
+            Token::Semicolon,
+            Token::Let,
+            Token::Identifier(String::from("result")),
+            Token::Assign,
+            Token::Identifier(String::from("add")),
+            Token::LeftParenthesis,
+            Token::Identifier(String::from("five")),
+            Token::Comma,
+            Token::Identifier(String::from("ten")),
+            Token::RightParenthesis,
+            Token::Semicolon,
+        ];
+
+        let tokens = lexer.into_iter().collect::<Vec<Token>>();
+
+        assert_eq!(expected_tokens, tokens);
+    }
 }
