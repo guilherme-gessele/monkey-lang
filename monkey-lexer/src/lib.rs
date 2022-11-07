46d72136283fbca92fa74c1335f4fe504c55fba2
guilherme-gessele <gagessele@outlook.com.br>
2022-11-06 21:29:21 -0300

    extended keywords and operators

--- monkey-lexer/src/lib.rs
+++ monkey-lexer/src/lib.rs
@@ -1,5 +1,29 @@
 use std::{iter::Peekable, str::Chars};
 
+const ASSIGN: char = '=';
+const PLUS: char = '+';
+const MINUS: char = '-';
+const BANG: char = '!';
+const SLASH: char = '/';
+const ASTERISK: char = '*';
+const LESS_THAN: char = '<';
+const GREATER_THAN: char = '>';
+
+const LEFT_PARENTHESIS: char = '(';
+const RIGHT_PARENTHESIS: char = ')';
+const LEFT_BRACE: char = '{';
+const RIGHT_BRACE: char = '}';
+const COMMA: char = ',';
+const SEMICOLON: char = ';';
+
+const FUNCTION: &'static str = "fn";
+const LET: &'static str = "let";
+const TRUE: &'static str = "true";
+const FALSE: &'static str = "false";
+const IF: &'static str = "if";
+const ELSE: &'static str = "else";
+const RETURN: &'static str = "return";
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Token {
     Illegal,
@@ -12,6 +36,14 @@ pub enum Token {
     // Operators
     Assign,
     Plus,
+    Minus,
+    Bang,
+    Slash,
+    Asterisk,
+    LessThan,
+    GreatherThan,
+    Equals,
+    NotEquals,
 
     // Delimiters
     Comma,
@@ -25,6 +57,11 @@ pub enum Token {
     // Keywords
     Let,
     Function,
+    If,
+    Else,
+    Return,
+    True,
+    False,
 }
 
 pub struct Lexer<'lexer> {
@@ -55,14 +92,47 @@ impl<'lexer> Iterator for Lexer<'lexer> {
         let keyword = self.input.next();
 
         match keyword {
-            Some('=') => Some(Token::Assign),
-            Some('+') => Some(Token::Plus),
-            Some('(') => Some(Token::LeftParenthesis),
-            Some(')') => Some(Token::RightParenthesis),
-            Some('{') => Some(Token::LeftBrace),
-            Some('}') => Some(Token::RightBrace),
-            Some(',') => Some(Token::Comma),
-            Some(';') => Some(Token::Semicolon),
+            Some(ASSIGN) => {
+
+                if let Some(p) = self.input.peek() {
+                    
+                    if p == &ASSIGN {
+                        self.input.next();
+                        return Some(Token::Equals);
+                    }
+                    
+                    return Some(Token::Assign);
+                }
+
+                Some(Token::Illegal)
+            },
+            Some(PLUS) => Some(Token::Plus),
+            Some(MINUS) => Some(Token::Minus),
+            Some(BANG) => {
+
+                if let Some(p) = self.input.peek() {
+                    
+                    if p == &ASSIGN {
+                        self.input.next();
+                        return Some(Token::NotEquals);
+                    }
+                    
+                    return Some(Token::Bang);
+                }
+
+                Some(Token::Illegal)
+            } ,
+            Some(SLASH) => Some(Token::Slash),
+            Some(ASTERISK) => Some(Token::Asterisk),
+            Some(LESS_THAN) => Some(Token::LessThan),
+            Some(GREATER_THAN) => Some(Token::GreatherThan),
+
+            Some(LEFT_PARENTHESIS) => Some(Token::LeftParenthesis),
+            Some(RIGHT_PARENTHESIS) => Some(Token::RightParenthesis),
+            Some(LEFT_BRACE) => Some(Token::LeftBrace),
+            Some(RIGHT_BRACE) => Some(Token::RightBrace),
+            Some(COMMA) => Some(Token::Comma),
+            Some(SEMICOLON) => Some(Token::Semicolon),
             Some(c) if c.is_digit(10) => {
                 let str = self.read_until(c, |c| c.is_digit(10));
 
@@ -72,8 +142,13 @@ impl<'lexer> Iterator for Lexer<'lexer> {
                 let str = self.read_until(c, |c| c.is_ascii_alphabetic());
 
                 match str.as_str() {
-                    "let" => Some(Token::Let),
-                    "fn" => Some(Token::Function),
+                    LET => Some(Token::Let),
+                    FUNCTION => Some(Token::Function),
+                    TRUE => Some(Token::True),
+                    FALSE => Some(Token::False),
+                    IF => Some(Token::If),
+                    ELSE => Some(Token::Else),
+                    RETURN => Some(Token::Return),
                     _ => Some(Token::Identifier(str)),
                 }
             }
@@ -115,9 +190,11 @@ mod tests {
     fn basic_code() {
         let input = "let five = 5;
         let ten = 10;
+        
         let add = fn(x, y) {
-        x + y;
+            x + y;
         };
+        
         let result = add(five, ten);
         ";
 
@@ -166,4 +243,121 @@ mod tests {
 
         assert_eq!(expected_tokens, tokens);
     }
+
+    #[test]
+    fn extended_keywords_and_operators() {
+        let input = "let five = 5;
+        
+        let ten = 10;
+        
+        let add = fn(x, y) {
+            x + y;
+        };
+
+        let result = add(five, ten);
+        
+        !-/*5;
+        5 < 10 > 5;
+
+        if (5 < 10) {
+            return true;
+        } else {
+            return false;
+        }
+
+        10 == 10;
+
+        10 != 9;
+        ";
+
+        let lexer = Lexer::new(input);
+
+        let expected_tokens = vec![
+            Token::Let,
+            Token::Identifier(String::from("five")),
+            Token::Assign,
+            Token::Integer(5),
+            Token::Semicolon,
+
+            Token::Let,
+            Token::Identifier(String::from("ten")),
+            Token::Assign,
+            Token::Integer(10),
+            Token::Semicolon,
+
+            Token::Let,
+            Token::Identifier(String::from("add")),
+            Token::Assign,
+            Token::Function,
+            Token::LeftParenthesis,
+            Token::Identifier(String::from("x")),
+            Token::Comma,
+            Token::Identifier(String::from("y")),
+            Token::RightParenthesis,
+            Token::LeftBrace,
+            Token::Identifier(String::from("x")),
+            Token::Plus,
+            Token::Identifier(String::from("y")),
+            Token::Semicolon,
+            Token::RightBrace,
+            Token::Semicolon,
+            Token::Let,
+            Token::Identifier(String::from("result")),
+            Token::Assign,
+            Token::Identifier(String::from("add")),
+            Token::LeftParenthesis,
+            Token::Identifier(String::from("five")),
+            Token::Comma,
+            Token::Identifier(String::from("ten")),
+            Token::RightParenthesis,
+            Token::Semicolon,
+
+            Token::Bang,
+            Token::Minus,
+            Token::Slash,
+            Token::Asterisk,
+            Token::Integer(5),
+            Token::Semicolon,
+
+            Token::Integer(5),
+            Token::LessThan,
+            Token::Integer(10),
+            Token::GreatherThan,
+            Token::Integer(5),
+            Token::Semicolon,
+
+            Token::If,
+            Token::LeftParenthesis,
+            Token::Integer(5),
+            Token::LessThan,
+            Token::Integer(10),
+            Token::RightParenthesis,
+            Token::LeftBrace,
+            Token::Return,
+            Token::True,
+            Token::Semicolon,
+            Token::RightBrace,
+            Token::Else,
+            Token::LeftBrace,
+            Token::Return,
+            Token::False,
+            Token::Semicolon,
+            Token::RightBrace,
+
+            Token::Integer(10),
+            Token::Equals,
+            Token::Integer(10),
+            Token::Semicolon,
+
+            Token::Integer(10),
+            Token::NotEquals,
+            Token::Integer(9),
+            Token::Semicolon,
+        ];
+
+        let tokens = lexer.into_iter().collect::<Vec<Token>>();
+
+        assert_eq!(expected_tokens, tokens);
+    }
+
 }
